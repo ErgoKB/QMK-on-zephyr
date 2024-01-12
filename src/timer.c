c53fb5993779202ad97c411fd37814f1c1409f43
LSChyi <alan81920@gmail.com>
2024-01-12 19:50:44 +0800

    implement more timer function for mouse

--- src/timer.c
+++ src/timer.c
@@ -1,3 +1,5 @@
+#include "timer.h"
+
 #include <zephyr/kernel.h>
 
 void timer_init(void) {}
@@ -5,3 +7,11 @@ void timer_init(void) {}
 uint16_t timer_read(void) { return (uint16_t)k_uptime_get_32(); }
 
 uint32_t timer_read32(void) { return k_uptime_get_32(); }
+
+uint16_t timer_elapsed(uint16_t last) {
+  return TIMER_DIFF_16(timer_read(), last);
+};
+
+uint32_t timer_elapsed32(uint32_t last) {
+  return TIMER_DIFF_32(timer_read32(), last);
+};
