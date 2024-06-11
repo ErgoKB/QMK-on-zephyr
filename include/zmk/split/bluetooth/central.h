3fa382ff84194207f95d1e853731e4923c13b8de
LSChyi <alan81920@gmail.com>
2024-06-11 18:52:01 +0800

    add clear bonding keys call

--- include/zmk/split/bluetooth/central.h
+++ include/zmk/split/bluetooth/central.h
@@ -3,6 +3,7 @@
 #include <stdint.h>
 
 void clear_bond();
+void clear_bond_secrets();
 void set_peripheral_addr(uint8_t *data, int idx);
 void clear_peripheral(int idx);
 void get_peripheral_addr(uint8_t *data, int idx);
