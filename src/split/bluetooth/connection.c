bc2b6d706b9d33e5d18b4430a71b55f3ea2979d3
LSChyi <alan81920@gmail.com>
2025-04-12 18:58:47 +0800

    split: connection: refactor connection logic
    Use the new slot API in connection process. Also rewrite and refine the
    discovery logic by early return the function, and directly passing the
    peripheral slot in static functions to save unnecessary get connection and
    checks.

--- src/split/bluetooth/connection.c
+++ src/split/bluetooth/connection.c
@@ -13,24 +13,20 @@ K_MSGQ_DEFINE(key_queue, sizeof(struct key_event), 30, 1);
 static const struct bt_uuid_128 split_service_uuid =
     BT_UUID_INIT_128(ZMK_SPLIT_BT_SERVICE_UUID);
 
-static bool is_scanning = false;
 static bool is_provisioning = true;
 
-static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
-                         struct net_buf_simple *ad);
+static void find_split_device(const bt_addr_le_t *addr, int8_t rssi,
+                              uint8_t type, struct net_buf_simple *ad);
 static bool parse_device_data(struct bt_data *data, void *user_data);
 static bool try_connect_split_device(const bt_addr_le_t *addr,
                                      struct peripheral_slot *slot);
-static int split_central_process_connection(struct bt_conn *conn,
-                                            struct peripheral_slot *slot);
 static uint8_t
 split_central_service_discovery_func(struct bt_conn *conn,
                                      const struct bt_gatt_attr *attr,
                                      struct bt_gatt_discover_params *params);
-static uint8_t
-split_central_chrc_discovery_func(struct bt_conn *conn,
-                                  const struct bt_gatt_attr *attr,
-                                  struct bt_gatt_discover_params *params);
+static uint8_t split_central_chrc_discovery_func(
+    struct bt_conn *conn, const struct bt_gatt_attr *attr,
+    struct bt_gatt_discover_params *params, struct peripheral_slot *slot);
 
 static uint8_t split_central_chrc_discovery_func_wrapper(
     struct bt_conn *conn, const struct bt_gatt_attr *attr,
@@ -43,117 +39,162 @@ split_central_notify_func(struct bt_conn *conn,
 
 void set_is_provisioning(bool val) { is_provisioning = val; }
 
-void security_changed(struct bt_conn *conn, bt_security_t level,
-                      enum bt_security_err err) {
-  if (!err) {
-    LOG_INF("Changed security success");
-    struct peripheral_slot *slot = get_slot_by_connection(conn);
-    if (split_central_process_connection(conn, slot)) {
-      // trigger a disconnect to also trigger start scan
-      bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
-    }
-    return;
-  }
-  LOG_ERR("Change security failed, unpair");
-  bt_unpair(BT_ID_DEFAULT, bt_conn_get_dst(conn));
-}
-
-bool split_central_connected_work(struct bt_conn *conn, uint8_t conn_err) {
-  char addr[BT_ADDR_LE_STR_LEN];
-  bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
-
-  if (conn_err) {
-    LOG_ERR("Failed to connect to %s (%u)", addr, conn_err);
-    return false;
-  }
-
-  LOG_DBG("Connected: %s", addr);
-
-  struct bt_conn_info conn_info;
-  if (bt_conn_get_info(conn, &conn_info)) {
-    LOG_ERR("Failed to get connection info");
-    return false;
-  }
-
-  if (conn_info.role == BT_CONN_ROLE_CENTRAL) {
-    LOG_INF("Role is central, setting security");
-    int err = bt_conn_set_security(conn, BT_SECURITY_L2);
-    if (err) {
-      LOG_ERR("Failed to set security; %d", err);
-      return false;
-    }
-  }
-
-  struct peripheral_slot *slot = get_slot_by_connection(conn);
-  if (slot == NULL) {
-    LOG_ERR("Cannot find corresponding slot");
-    return false;
-  }
-
-  confirm_peripheral_slot_conn(conn);
-  return true;
-}
-
+/*
+ * split_central_disconnected performs device disconnected clean up, and
+ * **triggers** running scanning (since there must be new empty slot).
+ * The slot is released in this call.
+ * This call shall not fail (or just fail with ignorable error).
+ */
 void split_central_disconnected(struct bt_conn *conn, uint8_t reason) {
   char addr[BT_ADDR_LE_STR_LEN];
+  struct peripheral_slot *slot;
+  uint32_t position;
+  struct key_event ev;
+
+  // No slot found, this connection is not in our split processing flow.
+  if (get_slot_by_connection(conn, &slot)) {
+    return;
+  }
 
   bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
   LOG_DBG("Disconnected: %s (reason %d)", addr, reason);
 
-  struct peripheral_slot *slot = get_slot_by_connection(conn);
   for (int i = 0; i < POSITION_STATE_DATA_LEN; i++) {
     for (int j = 0; j < 8; j++) {
       if (slot->position_state[i] & BIT(j)) {
-        uint32_t position = (i * 8) + j;
-        struct key_event ev = {
-            .position = position,
-            .pressed = false,
-        };
+        position = (i * 8) + j;
+        ev.position = position;
+        ev.pressed = false;
         k_msgq_put(&key_queue, &ev, K_NO_WAIT);
       }
     }
   }
 
-  release_peripheral_slot_for_conn(conn);
+  release_peripheral_slot(slot);
   bt_unpair(BT_ID_DEFAULT, bt_conn_get_dst(conn));
 
   start_scan();
 }
 
+/*
+ * split_central_connected just reserves a slot for the connection. The
+ * connection need to go through the security change process. Only if security
+ * change process passed, we then consider the device is online and is working.
+ *
+ * For stability, we will only start the scanning of the next device until the
+ * current complete process is completed. If any error encountered during the
+ * process, the device is considered offline, and corresponding resources should
+ * be released and restored.
+ *
+ * As such, the only thing this callback should do is just initiate the security
+ * change call.
+ */
 void split_central_connected(struct bt_conn *conn, uint8_t conn_err) {
-  if (!split_central_connected_work(conn, conn_err)) {
-    release_peripheral_slot_for_conn(conn);
+  char addr[BT_ADDR_LE_STR_LEN];
+  bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+  struct peripheral_slot *slot;
+
+  // No slot found, this connection is not in our split processing flow.
+  if (get_slot_by_connection(conn, &slot)) {
+    return;
   }
+
+  bt_addr_le_to_str(bt_conn_get_dst(conn), addr, sizeof(addr));
+  /*
+   * The connection slot is reserved during the scanning call, so when
+   * encountering any error in this callback, the slot must be released.
+   */
+  if (conn_err) {
+    LOG_ERR("Failed to connect to %s (%u)", addr, conn_err);
+    goto connected_drop_this_connection;
+  }
+
+  LOG_DBG("Connected: %s", addr);
+
+  int err = bt_conn_set_security(conn, BT_SECURITY_L2);
+  if (err) {
+    LOG_ERR("Failed to set security; %d", err);
+    goto connected_drop_this_connection;
+  }
+
+  return;
+
+connected_drop_this_connection:
+  bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+}
+
+/*
+ * security_changed is the callback that after bonding is processed. In our
+ * split connection flow, it is not completed yet. Only until the GATT discovery
+ * is complete, we can make sure the connected device contains the desire GATT
+ * service we watn, we then consider the device is online and ready, then we can
+ * start a new scanning process.
+ *
+ * As such, the only thing this callback should do is just trigger the GATT
+ * discovery.
+ */
+void security_changed(struct bt_conn *conn, bt_security_t level,
+                      enum bt_security_err err) {
+  struct peripheral_slot *slot;
+  int ret;
+
+  // No slot found, this connection is not in our split processing flow.
+  if (get_slot_by_connection(conn, &slot)) {
+    return;
+  }
+
+  if (err) {
+    LOG_ERR("Change security failed");
+    goto security_changed_drop_this_connection;
+  }
+
+  LOG_DBG("Changed security success");
+
+  slot->discover_params.uuid = &split_service_uuid.uuid;
+  slot->discover_params.func = split_central_service_discovery_func;
+  slot->discover_params.start_handle = 0x0001;
+  slot->discover_params.end_handle = 0xffff;
+  slot->discover_params.type = BT_GATT_DISCOVER_PRIMARY;
+
+  ret = bt_gatt_discover(slot->conn, &slot->discover_params);
+  if (ret) {
+    LOG_ERR("Failed to perform GATT discovery: %d", err);
+    goto security_changed_drop_this_connection;
+  }
+  return;
+
+security_changed_drop_this_connection:
+  bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
 }
 
 void start_scan(void) {
-  if (is_scanning) {
-    LOG_DBG("Already scanning, skip");
-    return;
-  }
+  uint32_t options = BT_LE_SCAN_OPT_FILTER_DUPLICATE;
+  int err;
 
   if (!has_open_slot()) {
     LOG_DBG("No empty slot, no need to scan");
     return;
   }
-  uint32_t options = BT_LE_SCAN_OPT_FILTER_DUPLICATE;
   if (!is_provisioning) {
     options |= BT_LE_SCAN_OPT_FILTER_ACCEPT_LIST;
   }
-  int err = bt_le_scan_start(BT_LE_SCAN_PARAM(BT_LE_SCAN_TYPE_PASSIVE, options,
-                                              BT_GAP_SCAN_FAST_INTERVAL,
-                                              BT_GAP_SCAN_FAST_WINDOW),
-                             device_found);
-  if (err) {
+
+  err = bt_le_scan_start(BT_LE_SCAN_PARAM(BT_LE_SCAN_TYPE_PASSIVE, options,
+                                          BT_GAP_SCAN_FAST_INTERVAL,
+                                          BT_GAP_SCAN_FAST_WINDOW),
+                         find_split_device);
+  if (err && err != -EBUSY) {
     LOG_ERR("Scanning failed to start (err %d)", err);
-    return;
   }
-  is_scanning = true;
   LOG_DBG("Scanning successfully");
 }
 
-static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
-                         struct net_buf_simple *ad) {
+static void find_split_device(const bt_addr_le_t *addr, int8_t rssi,
+                              uint8_t type, struct net_buf_simple *ad) {
+  bool is_split_service = false;
+  int err;
+  struct peripheral_slot *slot;
+
   if ((type != BT_GAP_ADV_TYPE_ADV_IND &&
        type != BT_GAP_ADV_TYPE_ADV_DIRECT_IND)) {
     return;
@@ -169,32 +210,36 @@ static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
     }
   }
   */
-  bool is_split_service = false;
   bt_data_parse(ad, parse_device_data, (void *)&is_split_service);
   if (!is_split_service) {
     return;
   }
 
-  int err = bt_le_scan_stop();
+  /*
+   * We found a possible split device, now stop scanning, reserve the radio
+   * until we complete the processing with this split device.
+   */
+  err = bt_le_scan_stop();
   if (err) {
     LOG_ERR("Stop LE scan failed (err %d)", err);
     return;
   }
-  is_scanning = false;
 
-  int slot_idx = reserve_peripheral_slot(addr);
-  if (slot_idx < 0) {
+  err = request_peripheral_slot(addr, &slot);
+  if (err) {
     LOG_DBG("Failed to reserve slot");
-    start_scan();
-    return;
+    goto device_found_revert_scanning;
   }
 
-  struct peripheral_slot *slot = &peripherals[slot_idx];
   if (!try_connect_split_device(addr, slot)) {
     LOG_DBG("Failed to connect to slot");
-    release_peripheral_slot(slot_idx);
-    start_scan();
+    release_peripheral_slot(slot);
+    goto device_found_revert_scanning;
   }
+  return;
+
+device_found_revert_scanning:
+  start_scan();
 }
 
 static bool parse_device_data(struct bt_data *data, void *user_data) {
@@ -238,7 +283,7 @@ static bool try_connect_split_device(const bt_addr_le_t *addr,
   }
   struct bt_le_conn_param *param = BT_LE_CONN_PARAM(0x0006, 0x0006, 0, 40);
 
-  LOG_DBG("Initiating new connnection");
+  LOG_DBG("Initiating new connection");
 
   int err = bt_conn_le_create(addr, BT_CONN_LE_CREATE_CONN, param, &slot->conn);
   if (err) {
@@ -249,31 +294,15 @@ static bool try_connect_split_device(const bt_addr_le_t *addr,
   return true;
 }
 
-static int split_central_process_connection(struct bt_conn *conn,
-                                            struct peripheral_slot *slot) {
-  slot->discover_params.uuid = &split_service_uuid.uuid;
-  slot->discover_params.func = split_central_service_discovery_func;
-  slot->discover_params.start_handle = 0x0001;
-  slot->discover_params.end_handle = 0xffff;
-  slot->discover_params.type = BT_GATT_DISCOVER_PRIMARY;
-
-  int err = bt_gatt_discover(slot->conn, &slot->discover_params);
-  if (err) {
-    LOG_ERR("Discover failed(err %d)", err);
-    return err;
-  }
-  return 0;
-}
-
 static uint8_t
 split_central_notify_func(struct bt_conn *conn,
                           struct bt_gatt_subscribe_params *params,
                           const void *data, uint16_t length) {
-  struct peripheral_slot *slot = get_slot_by_connection(conn);
+  struct peripheral_slot *slot;
 
-  if (slot == NULL) {
+  if (get_slot_by_connection(conn, &slot)) {
     LOG_ERR("No peripheral state found for connection");
-    return BT_GATT_ITER_CONTINUE;
+    return BT_GATT_ITER_STOP;
   }
 
   if (!data) {
@@ -320,8 +349,8 @@ split_central_service_discovery_func(struct bt_conn *conn,
 
   LOG_DBG("[ATTRIBUTE] handle %u", attr->handle);
 
-  struct peripheral_slot *slot = get_slot_by_connection(conn);
-  if (slot == NULL) {
+  struct peripheral_slot *slot;
+  if (get_slot_by_connection(conn, &slot)) {
     LOG_ERR("No peripheral state found for connection");
     return BT_GATT_ITER_STOP;
   }
@@ -347,13 +376,8 @@ split_central_service_discovery_func(struct bt_conn *conn,
   return BT_GATT_ITER_STOP;
 }
 
-static void split_central_subscribe(struct bt_conn *conn) {
-  struct peripheral_slot *slot = get_slot_by_connection(conn);
-  if (slot == NULL) {
-    LOG_ERR("No peripheral state found for connection");
-    return;
-  }
-
+static void split_central_subscribe(struct bt_conn *conn,
+                                    struct peripheral_slot *slot) {
   int err = bt_gatt_subscribe(conn, &slot->subscribe_params);
   switch (err) {
   case -EALREADY:
@@ -369,10 +393,9 @@ static void split_central_subscribe(struct bt_conn *conn) {
   }
 }
 
-static uint8_t
-split_central_chrc_discovery_func(struct bt_conn *conn,
-                                  const struct bt_gatt_attr *attr,
-                                  struct bt_gatt_discover_params *params) {
+static uint8_t split_central_chrc_discovery_func(
+    struct bt_conn *conn, const struct bt_gatt_attr *attr,
+    struct bt_gatt_discover_params *params, struct peripheral_slot *slot) {
   if (!attr) {
     LOG_DBG("Discover complete");
     return BT_GATT_ITER_STOP;
@@ -380,12 +403,6 @@ split_central_chrc_discovery_func(struct bt_conn *conn,
 
   if (!attr->user_data) {
     LOG_ERR("Required user data not passed to discovery");
-    return BT_GATT_ITER_STOP;
-  }
-
-  struct peripheral_slot *slot = get_slot_by_connection(conn);
-  if (slot == NULL) {
-    LOG_ERR("No peripheral state found for connection");
     return BT_GATT_ITER_STOP;
   }
 
@@ -404,7 +421,7 @@ split_central_chrc_discovery_func(struct bt_conn *conn,
     slot->subscribe_params.value_handle = bt_gatt_attr_value_handle(attr);
     slot->subscribe_params.notify = split_central_notify_func;
     slot->subscribe_params.value = BT_GATT_CCC_NOTIFY;
-    split_central_subscribe(conn);
+    split_central_subscribe(conn, slot);
   } else if (!bt_uuid_cmp(
                  ((struct bt_gatt_chrc *)attr->user_data)->uuid,
                  BT_UUID_DECLARE_128(ZMK_SPLIT_BT_CHAR_RUN_BEHAVIOR_UUID))) {
@@ -421,16 +438,31 @@ split_central_chrc_discovery_func(struct bt_conn *conn,
 static uint8_t split_central_chrc_discovery_func_wrapper(
     struct bt_conn *conn, const struct bt_gatt_attr *attr,
     struct bt_gatt_discover_params *params) {
-  uint8_t res = split_central_chrc_discovery_func(conn, attr, params);
-  if (res == BT_GATT_ITER_STOP) {
-    struct peripheral_slot *slot = get_slot_by_connection(conn);
-    bool subscribed =
-        (slot->run_behavior_handle && slot->subscribe_params.value_handle);
-    if (!subscribed) {
-      bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
-    } else {
-      start_scan();
-    }
+  struct peripheral_slot *slot;
+  uint8_t res;
+
+  if (get_slot_by_connection(conn, &slot)) {
+    // the slot somehow becomes unavailable, disconnect with this device
+    bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+    return BT_GATT_ITER_STOP;
   }
-  return res;
+
+  res = split_central_chrc_discovery_func(conn, attr, params, slot);
+  if (res != BT_GATT_ITER_STOP) {
+    return res;
+  }
+
+  /*
+   * At this point, we have completely iterated the GATT services on this
+   * device, so if it is still not subscribed, something went wrong on this
+   * discover, and we have to disconnect with this device.
+   */
+  bool subscribed =
+      (slot->run_behavior_handle && slot->subscribe_params.value_handle);
+  if (!subscribed) {
+    bt_conn_disconnect(conn, BT_HCI_ERR_REMOTE_USER_TERM_CONN);
+  }
+  // If there is still empty slots, restart the scanning
+  start_scan();
+  return BT_GATT_ITER_STOP;
 }
